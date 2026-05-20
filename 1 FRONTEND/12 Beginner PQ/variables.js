4d502a89cf8506fe0f689f31ee98b22ff0dac84b
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-05-20 22:17:12 +0200

    Created some Variables in JS

---
+++ 1 FRONTEND/12 Beginner PQ/variables.js
@@ -0,0 +1,39 @@
+/*
+1. Create a variable called `studentName` and store your name in it.
+2. Create a variable `age` and print it.
+3. Create two variables and swap their values.
+4. Create a constant variable for `PI` and print it.
+5. Declare a variable without assigning a value and print it.
+6. Create a variable `score` and increase it by 10.
+7. Create three variables for first name, last name, and full name.
+*/
+
+// 1
+var studentName = "Mohammad Ayan Asim";
+
+// 2
+var age = 17;
+console.log(age);
+
+// 3
+var v1 = 5;
+var v2 = 4;
+[v1, v2] = [v2, v1];
+console.log(v1, v2);
+
+// 4
+const PI = 3.14;
+console.log(PI);
+
+// 5
+var a;
+console.log(a);
+
+// 6
+var score = 5;
+score += 10;
+
+// 7
+var firstName = "Mohammad";
+var secondName = "Ayan";
+var fullName = "Mohammad Ayan";
