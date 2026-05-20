41d5adedf1d395e9d21c5c18c1ab907a3107078e
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-05-20 22:10:58 +0200

    Write console methods

---
+++ 1 FRONTEND/12 Beginner PQ/consoleBasics.js
@@ -0,0 +1,13 @@
+/*
+1. Print `"Hello JavaScript"` in the console.
+2. Print your name, age, and city using one `console.log()`.
+3. Print a warning message using `console.warn()`.
+4. Print an error message using `console.error()`.
+5. Use `console.table()` to display an array of 5 numbers.
+*/
+
+console.log('Hello JavaScript');
+console.log('Mohammad Ayan Asim', 17, 'Johannesburg');
+console.warn("It's Warning");
+console.error("It's Error");
+console.table([1, 2, 3, 4, 5]);
