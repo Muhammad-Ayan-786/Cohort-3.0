3b4804f649ac38518ac68ac17d63566b87b60c2f
Muhammad Ayan <mohd.ayanasim.786@gmail.com>
2026-09-20 19:02:25 +0200

    Validate req using express validator

---
+++ 2-BACKEND/D15-Express_Validator/src/validators/auth.validator.js
@@ -0,0 +1,26 @@
+import { body, validationResult } from 'express-validator'
+
+export const registerValidation = [
+  body('email')
+    .exists().withMessage('Email is required')
+    .isEmail().withMessage('Invalid email'),
+  body('phone')
+    .exists().withMessage('Phone number is required')
+    .isLength({ min: 10, max: 10 }).withMessage('Invalid phone number'),
+  body('password')
+    .exists().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+
+  (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "Invalid Request",
+        errors: errors.array()
+      })
+    }
+
+    next()
+  }
+]
